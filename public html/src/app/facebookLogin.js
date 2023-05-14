9d7f589caddd5f4ab5fe2de73877c79b83f64c9f
SaúlTejeda117 <79871748+saulTejeda117@users.noreply.github.com>
2023-05-13 21:20:10 -0600

    Add files via upload

--- public html/src/app/facebookLogin.js
+++ public html/src/app/facebookLogin.js
@@ -1,3 +1,31 @@
+// import { FacebookAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
+// import { auth } from "./firebase.js";
+// import { showMessage } from "./showMessage.js";
+
+// const facebookButton = document.querySelector('#facebookLogin');
+
+// facebookButton.addEventListener('click', async e => {
+//   e.preventDefault();
+
+//   const provider = new FacebookAuthProvider();
+
+//   try {
+//     const credentials = await signInWithPopup(auth, provider)
+//     console.log(credentials);
+//     console.log("facebook sign in");
+    
+//     // Close the login modal
+//     const modal = bootstrap.Modal.getInstance(facebookButton.closest('.modal'));
+//     modal.hide();
+
+//     // show welcome message
+//     showMessage("Welcome" + credentials.user.email);
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// })
+
 import { FacebookAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
 import { auth } from "./firebase.js";
 import { showMessage } from "./showMessage.js";
@@ -10,9 +38,17 @@ facebookButton.addEventListener('click', async e => {
   const provider = new FacebookAuthProvider();
 
   try {
-    const credentials = await signInWithPopup(auth, provider)
+    const credentials = await signInWithPopup(auth,provider)
     console.log(credentials);
     console.log("facebook sign in");
+
+    const result = await signInWithPopup(auth, provider)
+      window.addEventListener('beforeunload', () => {
+        result.user.getIdToken().then((token) => {
+          // use the token to log in
+          console.log('User logged in with Facebook:', token);
+        });
+    });
     
     // Close the login modal
     const modal = bootstrap.Modal.getInstance(facebookButton.closest('.modal'));
@@ -25,3 +61,5 @@ facebookButton.addEventListener('click', async e => {
   }
 
 })
+
+// Agregar este código para asegurarse de que se abra la ventana emergente al hacer clic en el botón de inicio de sesión de Facebook
